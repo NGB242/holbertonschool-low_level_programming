b25dc901f6399c22bff30d65cfac5e0181010efa
NGB242 <ngbwarren@gmail.com>
2024-10-26 10:31:07 +0200

    affiche le resultat

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,16 @@
 /**
  *
  * main - Affiche les resultats
+ *
  * Return 0 (success)
  */
 int main(void)
-{	int n;
+{	
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
