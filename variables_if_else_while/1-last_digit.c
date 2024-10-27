7a37cfee4a71db58bde2a3d274f8e3f9a1ad14c7
NGB242 <ngbwarren@gmail.com>
2024-10-27 09:43:09 +0100

    display result of 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 int last_digit = n % 10;
-printf("Last digit of %d id", n);
+{printf("Last digit of %d id", n);
+}
 if (last_digit > 5)
 {	printf("and is greater than 5\n");
 }
