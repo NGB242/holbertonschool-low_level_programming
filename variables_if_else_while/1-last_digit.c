12d689cfad4734486fe3b9b3a8f23399a88ba64b
NGB242 <ngbwarren@gmail.com>
2024-10-27 09:38:23 +0100

    display result of 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,14 +10,16 @@ int main(void)
 {	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 7 > 5)
-{	printf("%d is greater than 5\n", n, 7);
+int last_digit = n % 10;
+printf("Last digit of %d id", n);
+if (last_digit > 5)
+{	printf("and is greater than 5\n");
 }
-	else if (n %  == 0)
-{	printf("%d is zero\n", n);
+	else if (last_digit  == 0)
+{	printf(" and is 0\n");
 }
 	else
-{	printf("%d is less than 6 and not 0\n", n);
+{	printf(" and is less than 6 and not 0\n");
 }
 	return (0);
 }
