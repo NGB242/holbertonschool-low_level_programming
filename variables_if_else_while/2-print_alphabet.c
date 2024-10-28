a0ed024ba33c5cbbfe01ce16000b21c5a8a3a93e
NGB242 <ngbwarren@gmail.com>
2024-10-28 10:53:13 +0100

    display the result 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Affichebles resultats
- * Description: program that prints the alphabet in lowercase, followed by a new line
- *Return: Always 0 (success)
- */
+* main - Affichebles resultats
+* Description: program that prints the alphabet in lowercas
+*Return: Always 0 (success)
+*/
 int main(void)
-{
-	char low;
-	for (low = 'a'; low <= 'z'; low++) 
-{
-	putchar(low);
-}
-	putchar('\n');
 
-	return (0);
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+{
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
