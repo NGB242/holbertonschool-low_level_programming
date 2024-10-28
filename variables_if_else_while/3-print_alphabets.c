27a22274edd1198dc1fcd7b00baba989bd415bdf
NGB242 <ngbwarren@gmail.com>
2024-10-28 15:10:45 +0100

    display the result 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
 *main - Affiche les resultats
 *Return: Always 0 (success)
 */
 int main(void)
+
 {
-char low;
-for (low = 'a'; low <= 'z'; low++)
-{
-putchar(low);
-}
-for (low = 'A'; low <= 'z'; low++)
-{putchar(low);
-}
-{
-putchar('\n');
-}
-return (0);
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (low = 'A'; low <= 'Z'; low++)
+	{
+		putchar(low);
+		putchar('\n');
+	}
+	return (0);
 }
