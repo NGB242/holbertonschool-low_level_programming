1d8567b6b0841b00bed7511dd74f2e277c3a2f62
NGB242 <ngbwarren@gmail.com>
2024-10-28 15:19:13 +0100

    display the result 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+/**
+*main - Affiche les resultats
+*Description:
+*Return:Always 0 (success)
+*/
+	int main(void)
+{
+	char low;
+for (low = 'z'; low >= 'a'; low--)
+{	putchar(low);
+	putchar('\n');
+}
+	return (0)
