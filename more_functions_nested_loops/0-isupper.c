aa07491a52c5017f92d5b979bc9493ec60e0170f
NGB242 <ngbwarren@gmail.com>
2024-10-29 16:05:43 +0100

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+* main - Vérifie le code
+* Description:c vérifie le code 
+* Return 0 otherwise
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+	return 1;
+	return (0);
+}
