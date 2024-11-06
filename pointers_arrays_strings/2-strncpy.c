e336894fe23f675bc6f744376e1333a721c00c4d
NGB242 <ngbwarren@gmail.com>
2024-11-06 16:30:35 +0100

    display the result

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,12 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 	srclen++;
 
 	if (n > srclen)
-		n = srclen;
+	n = srclen;
 
 	src = start;
 
 	for (; i < n; i++)
-		*dest++ = *src++;
+	*dest++ = *src++;
 
 	return (temp);
 }
