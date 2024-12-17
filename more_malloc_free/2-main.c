6ebbbff2af24557338209045da1a52173fbb3695
NGB242 <ngbwarren@gmail.com>
2024-12-17 04:20:01 +0100

    add the result of the task 3

--- more_malloc_free/2-main.c
+++ more_malloc_free/2-main.c
@@ -12,22 +12,22 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
 printf("\n");
 }
 
@@ -36,6 +36,7 @@ printf("\n");
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 char *a;
@@ -60,25 +61,26 @@ return (0);
  *
  * Return: Nothing.
  */
+
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
--- more_malloc_free/3-main.c
+++ more_malloc_free/3-main.c
@@ -19,15 +19,15 @@ while (i < size)
 {
 if (i % 10)
 {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
 printf("\n");
 }
 
