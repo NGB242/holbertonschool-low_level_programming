c56209315a7252f3a9085e6826c377a5127684b5
NGB242 <ngbwarren@gmail.com>
2025-01-08 23:44:18 +0100

    add the result

---
+++ more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - ENTRY to test a function that prints the numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/** 
+ * print_list: prints all the elements of a list_t
+ * @h: pointer to list
+ * return: the number of nodes
+*/
+
+size_t print_list(const list_t *h);
+{
+    int count;
+    count = 0
+    while (h != NULL)
+{
+    printf("%d\n", h->n);
+    h = h->next;
+    count++;
+}
+
+if (str == NULL)
+print("%d[0]\n", str)
+return (count);
+}
