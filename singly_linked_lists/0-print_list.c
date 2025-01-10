138a7ddc9bb7956973911e1a3495b42742e9d2fe
NGB242 <ngbwarren@gmail.com>
2025-01-10 23:16:13 +0100

    display the result of the question

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,30 +1,31 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - Affiche touts les éléments d'une liste chaînée
+ * @h: Pointeur vers le premier nœud de la liste
  *
- * Return: The number of nodes in h.
+ * Description: Parcourt la liste `list_t` et affiche chaque nœud.
+ * Si `str` est NULL, affiche "[0] (nil)".
+ *
+ * Return: Le nombre total de nœuds.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+size_t compteur = 0;
+if (h == NULL)
+{
+printf("[0] (nil) \n");
+return (0);
+}
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+++compteur;
+}
+return (compteur);
 }
