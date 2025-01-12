209a92ef4ef2831aeda3b5921eb5306f06028d5b
NGB242 <ngbwarren@gmail.com>
2025-01-12 21:55:44 +0100

    display the result

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,24 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * add_dnodeint - Ajoute un nouveau noeud au début de la liste
- * @head: Le pointer du premier noeud de la liste
- * Return: La nouvelle liste, ou NULL si cela échoue.
- * @n: l'entier stocké dans le noeud à remplacer par l'entier en entrée.
+ * add_dnodeint - Ajoute un nouveau nœud au début d'une liste dlistint_t
+ * @head: Double pointeur vers la tête de la liste
+ * @n: Donnée à insérer dans le nouveau nœud
+ *
+ * Return: Adresse du nouveau nœud, ou NULL si l'allocation échoue
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *debutliste;
-debutliste = malloc(sizeof(dlistint_t));
-if (debutliste == NULL)
-return (NULL);
-debutliste->n = n;
-debutliste->next = *head;
-debutliste->prev = NULL;
-if (*head != NULL)
-(*head)->prev = debutliste;
-*head = debutliste;
-return (debutliste);
+	dlistint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
 }
