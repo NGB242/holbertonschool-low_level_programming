73829032f211b45600dff85114fdebd7f603a5f0
NGB242 <ngbwarren@gmail.com>
2025-01-13 04:07:12 +0100

    display the result of the taks

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
-
+char *_strcpy(char *dest, char *src);
 #endif /* MAIN_H */
