aafcbd95b26cbb569fcfd9fe818d32597af10272
NGB242 <ngbwarren@gmail.com>
2025-01-13 02:02:00 +0100

    display the result of the taks

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
- * print_name - Print a name from a function pointer
- * @name: char string.
- * @f: function pointer that takes a string argument
- * Fonction qui utilise fonction
+ * print_name - Appelle une fonction pour imprimer un nom
+ * @name: Pointeur vers le nom à imprimer
+ * @f: Pointeur vers une fonction qui imprime le nom
+ *
+ * Description: Si name et f ne sont pas NULL, la fonction f est appelée.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{	(f)(name);
+	if (name != NULL && f != NULL) /* Vérifie que les paramètres sont valides */
+	{
+		f(name); /* Appelle la fonction pointée par f avec name */
 	}
 }
