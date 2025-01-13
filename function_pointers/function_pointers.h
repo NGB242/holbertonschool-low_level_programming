1768d424eab74e60397da1d3599f1d97e16fa4d4
NGB242 <ngbwarren@gmail.com>
2025-01-13 02:14:49 +0100

    display the result of the taks

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,12 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stddef.h> 
-/* Déclaration de Prototype*/
 
-int _putchar(char c);
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
+
