7ac8dcd4545b7b8c2b5bba4107091ed5745a4451
NGB242 <ngbwarren@gmail.com>
2025-01-13 09:58:23 +0100

    display the result of the taks

--- file_io/main.h
+++ file_io/main.h
@@ -8,4 +8,5 @@ void error98(char *filename);
 void error99(char *filename);
 void subfunc_copy(char *file_from, char *file_to);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
